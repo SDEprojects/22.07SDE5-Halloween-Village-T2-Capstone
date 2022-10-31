cf0dcab9894ed45b7cf933909c906df3afa53e1c
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-10-31 14:36:34 -0700

    refactored fields in main, fixed bug pertaining to user moves displaying

--- src/main/java/com/halloween/Main.java
+++ src/main/java/com/halloween/Main.java
@@ -1,5 +1,6 @@
 package com.halloween;
 
+import static com.halloween.controller.Game.getDisplay;
 import static com.halloween.view.SoundEffects.muteSoundEffects;
 import static com.halloween.view.SoundEffects.playSound;
 import static com.halloween.view.SoundEffects.unmuteSoundEffects;
@@ -20,6 +21,8 @@ import javax.swing.SwingUtilities;
 
 public class Main {
   private static Game game;
+  private static String[] input;
+  private static TextParser textParser;
   public static Game getGame(){
     return game;
   }
@@ -41,7 +44,7 @@ public class Main {
 
 
     Game game = new Game(); // an instance of the Game model (MVC)
-    TextParser textParser = new TextParser(); // text parser used to parse the user input (command)
+    textParser = new TextParser(); // text parser used to parse the user input (command)
     boolean gameStarted = false; // variable used to decide whether a game has started or not
     // check if a game has started or not
     while (!gameStarted) {
@@ -87,7 +90,7 @@ public class Main {
       // show the current game's status
       game.showStatus();
       // parse the user input into an array of words
-      String[] input = textParser.userInput();
+      input = textParser.userInput();
 
       // check if the parsed user input matches a valid command and if so, invoke an appropriate
       // method related to the command
@@ -132,6 +135,15 @@ public class Main {
     } else {
       game.showLose();
       game.removeFiles();
+    }
+    getDisplay().printPlayNewGame();
+    playNewGame();
+  }
+
+  public static void playNewGame(){
+    String[] NewInput = textParser.userInput();
+    if(!NewInput[0].equals("n")) {
+    playGame(new Game(), textParser);
     }
   }
 
