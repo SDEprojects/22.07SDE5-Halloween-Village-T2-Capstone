ba70c89ec10e86f180ee0691c2ca5e956cb708c8
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-11-02 15:19:45 -0700

    AB#1406 map screen

--- src/main/java/com/halloween/controller/GUI.java
+++ src/main/java/com/halloween/controller/GUI.java
@@ -3,17 +3,21 @@ package com.halloween.controller;
 import com.halloween.model.Neighborhood;
 import com.halloween.model.Player;
 import com.halloween.view.GUI.TitleScreen;
+import com.halloween.view.GUI.mapScreen;
+import java.io.IOException;
 
 public class GUI {
 
   TitleScreen titleScreen;
+  mapScreen mapScreen;
   Game game;
   Player player;
   String currentLocation;
   Neighborhood neighborhood;
 
-  public GUI(Game game) {
+  public GUI(Game game) throws IOException {
     titleScreen = new TitleScreen(game);
+    mapScreen = new mapScreen(game);
   }
 
   public void startNewGame() {
--- src/main/java/com/halloween/view/GUI/mapScreen.java
+++ src/main/java/com/halloween/view/GUI/mapScreen.java
@@ -1,5 +1,43 @@
 package com.halloween.view.GUI;
 
-public class mapScreen {
+import com.halloween.controller.Game;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+
+public class mapScreen {
+  JFrame window;
+  private final Font BUTTON_FONT = new Font("Times New Roman", Font.PLAIN, 10);
+
+  public mapScreen(Game game) throws IOException {
+    window = new JFrame();
+    window.setSize(1500, 1500);
+    window.setVisible(true);
+//    window.setResizable(false);
+//    window.setLocationRelativeTo(null);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of the application
+    window.setLayout(null);
+    window.getContentPane().setBackground(Color.black);
+
+    JLabel label = new JLabel();
+    label.setIcon(new ImageIcon("src/main/resources/small_map.png"));
+    label.setBounds(80, 100, 1400, 500);
+
+    JButton backToGameButton = new JButton("BACK TO GAME");
+    backToGameButton.setFont(BUTTON_FONT);
+    backToGameButton.setBounds(650, 650, 200, 75);
+    backToGameButton.addActionListener(event -> {
+      System.out.println("this button should bring user back to game screen");
+    });
+
+    window.add(label);
+    window.add(backToGameButton);
+
+  }
 }
